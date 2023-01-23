c17032594b4736af2846bb1fd3922e2b992a8cd8
felixokolo <pheelix.info@gmail.com>
2023-01-23 21:27:48 +0100

    addition

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -6,11 +6,12 @@ Pagination
 
 from typing import Tuple
 
+
 def index_range(page: int,
                 page_size: int) -> Tuple[int, int]:
-        """
-        Calculation of start and end index of page
-        """
-        start_idx : int = (page - 1) * page_size
-        end_idx : int = page * page_size
-        return (start_idx, end_idx)
+    """
+    Calculation of start and end index of page
+    """
+    start_idx: int = (page - 1) * page_size
+    end_idx: int = page * page_size
+    return (start_idx, end_idx)
