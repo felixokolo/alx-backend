b0c7a4d32ba8372582f185e747afa07f14b666de
felixokolo <pheelix.info@gmail.com>
2023-01-31 15:11:39 +0100

    correction

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Flask-Babel app
+"""
+
+from flask_babel import Babel
+from flask import Flask
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config(object):
+    """
+    Configuration class
+    """
+    LANGUAGES = ["en", "fr"]
+
+    def get_locale():
+        return "en"
+
+    def get_timezone():
+        return "UTC"
+
+
+if __name__ == '__main__':
+    babel.get_locale = Config.get_locale()
+    babel.get_timezone = Config.get_timezone()
+    app.run('0.0.0.0')
